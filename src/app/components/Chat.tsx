d803222a5300b5d7e024eea749ddc286f61b7f8d
Jeffin <jeffinj787@gmail.com>
2024-04-15 22:17:28 +0100

    fix: add app title

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import UserAvatar from './UserAvatar';
-import AIAvatar from './AIAvatar';
 import UserMessage from './UserMessage';
 import AIMessage from './AIMessage';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex relative flex-col w-3/6 py-24 mx-auto stretch overflow-y-auto text-white h-screen">
+    <div className="flex relative flex-col gap-y-2 w-3/6 py-24 mx-auto stretch overflow-y-auto text-white h-screen">
+
+      <h1 className='text-2xl font-semibold underline text-emerald-600 mb-5'>OpenAI Chat</h1>
 
       <div className='flex flex-col gap-y-2 px-1'>
         {messages.map(message => (
